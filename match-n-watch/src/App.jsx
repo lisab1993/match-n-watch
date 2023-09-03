28e440e223c6fc0ccfdf6eb88b85417d40e6a2c2
lisab1993 <brown.lisa999@gmail.com>
2023-09-02 18:07:39 -0700

    deployment

---
+++ match-n-watch/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  
+
+  return (
+    <>
+    <div className="text-red-600">
+      Under construction, thank you for your patience  
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ match-n-watch/tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content:["./src/**/*.{js,jsx,cjs}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
